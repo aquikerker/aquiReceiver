f3a53a930a7bb9487680d5d20475af5b83a3d02f
Jordan Hsu <jordan00830@gmail.com>
2014-06-07 19:25:42 +0800

    Jordan's first testing for chromecast

---
+++ js/bonsai.js
@@ -0,0 +1,23 @@
+$(function(){
+	console.log(test1);
+	console.log(test1.HEAD);
+	console.log(test1.content);
+	console.log(test2);
+
+});
+
+
+
+
+
+
+
+var test1 = {"HEAD": 'menuList', 
+  "content": [{"dishID": 1, "name": '撒尿牛丸', "price": 200}, 
+            {"dishID": 2, "name": '醬爆牛丸', "price": 99999}]
+ };
+
+var test2 = {"HEAD": 'order',
+ "tableID": 5,
+ "content": [1,2]
+}
